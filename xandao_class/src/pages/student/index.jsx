b8344343e2b7d8f7518aa13528953fa4c7bf6600
Matheus Moraes <mateyc_2013@outlook.com>
2023-06-21 14:33:28 -0300

    imports

--- xandao_class/src/pages/student/index.jsx
+++ xandao_class/src/pages/student/index.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React from 'react';
+import {useState, useEffect} from 'react';
+import { db } from '../../services/firebaseConnection';
+import { addDoc, collection, getDocs, doc, deleteDoc} from 'firebase/firestore';
 
 const Student = () =>
 {
