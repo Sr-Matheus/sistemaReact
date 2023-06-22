6730120ca8230a1db0443b3e56b311c14b92baa9
Matheus Moraes <mateyc_2013@outlook.com>
2023-06-22 18:17:37 -0300

    navbar and home

--- xandao_class/src/components/nav/index.jsx
+++ xandao_class/src/components/nav/index.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import './navbar.css'
+import logo_pos from '../../resources/img/logo_pos.svg'
+import DashboardIcon from '../../resources/icons/dashboard.svg';
+import TeacherIcon from '../../resources/icons/teacher.svg';
+import StudentIcon from '../../resources/icons/student.svg';
 
 const MyNavBar = () =>
 {
     return(
         <>
+            <div className="navbar">
+                <img src={logo_pos} alt="Logo" className="logo" />
+                <nav>
+                    <ul className="nav-links">
+                        <li>
+                            <a href="/" className="nav-link">
+                            <img src={DashboardIcon} alt="Dashboard" className="icon" />
+                            Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/alunos" className="nav-link">
+                            <img src={StudentIcon} alt="Student" className="icon" />
+                            Alunos
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/professores" className="nav-link">
+                            <img src={TeacherIcon} alt="Teacher" className="icon" />
+                            Professores
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </>
     );
 }
--- xandao_class/src/pages/home/index.jsx
+++ xandao_class/src/pages/home/index.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import './home.css'
+import home from '../../resources/img/home.svg'
 
 const Home = () =>
 {
     return(
         <>
-            <p>Teste Home</p>
+            <main>
+                <h1>Bem-vindo!</h1>
+
+                <div className="body">
+                    <p>
+                        É com grande alegria que damos as boas-vindas a você em nossa plataforma dedicada à educação e colaboração entre professores e alunos. Aqui, você terá acesso a um ambiente dinâmico e interativo, projetado para facilitar o cadastro e gerenciamento de informações acadêmicas.
+                    </p>
+
+                    <img src={home} alt="" className="home_pic" />
+                </div>
+                
+            </main>
+            
         </>
     );
 }
