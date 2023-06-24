03c0e85f4e2f0e545ea7f9d67026e3475044a409
Matheus Moraes <mateyc_2013@outlook.com>
2023-06-23 23:00:59 -0300

    student

--- xandao_class/src/pages/student/add.jsx
+++ xandao_class/src/pages/student/add.jsx
@@ -11,16 +11,11 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import BadgeIcon from '@mui/icons-material/Badge';
 import CakeIcon from '@mui/icons-material/Cake';
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import PlaceIcon from '@mui/icons-material/Place';
-import { useNavigate } from 'react-router-dom';
 
 const bull = (<Box component="span" sx={{ display: 'flex', mx: '2px', transform: 'scale(0.8)'}}>•</Box>);
 
@@ -34,48 +29,45 @@ const StudentAdd = () =>
             <main>
               
                 <div className="body">
+
+                
+                <Paper elevation={24}> 
                     
-                    <Card className='card'>
+                    <Card variant="outlined" className='card'>
 
-                    <Typography variant="h3" component="div" className='card-title'>
-                            Cadastro de Alunos
-                    </Typography>
+                        <Typography variant="h3" component="div" className='card-title'>
+                                <b>Cadastro de Alunos</b>
+                        </Typography>
                     
-                    <CardContent className='card-content'>
+                        <CardContent className='card-content'>
 
-                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                    <DriveFileRenameOutlineIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                        <TextField id="input-with-sx" label="Nome" variant="standard" />
-                    </Box>
-
-                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                    <BadgeIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                        <TextField id="input-with-sx" label="CPF" variant="standard" />
-                    </Box>
-
-                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                    <CakeIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                        <TextField id="input-with-sx" label="Idade" variant="standard" />
-                    </Box>
-
-                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                    <AutoStoriesIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                        <TextField id="input-with-sx" label="Serie" variant="standard" />
-                    </Box>
-
-                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                    <PlaceIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                        <TextField id="input-with-sx" label="Endereço" variant="standard" />
-                    </Box>
-                        
-                        
-                    </CardContent>
-                    <CardActions className='card-actions'>
-                        <Button href='/alunos'>Cancelar</Button>
-                        <Button variant="contained">Cadastrar</Button>
-                    </CardActions>
-                </Card>
-
+                        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                            <DriveFileRenameOutlineIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                            <TextField id="input-with-sx" label="Nome" variant="standard" />
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                            <BadgeIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                            <TextField id="input-with-sx" label="CPF" variant="standard" />
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                            <CakeIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                            <TextField id="input-with-sx" label="Idade" variant="standard" />
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                            <AutoStoriesIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                            <TextField id="input-with-sx" label="Serie" variant="standard" />
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                            <PlaceIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                            <TextField id="input-with-sx" label="Endereço" variant="standard" />
+                        </Box>                            
+                        </CardContent>
+                            <CardActions className='card-actions'>
+                                <Button href='/alunos' variant="outlined" >Cancelar</Button>
+                                <Button variant="contained">Cadastrar</Button>
+                            </CardActions>
+                    </Card>
+                </Paper>
        
                 
                 </div>
